5d0ec9b8245c1ed371d8715bb34d4cc67617edf7
zabronax <zabronax@gmail.com>
2025-03-20 13:45:36 +0100

    pass get response asssertion

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.MapGet("/feedback", () => "Feedback Returned!");
 app.MapPost("/feedback", () => "Feedback Recieved!");
 
 app.Run();
